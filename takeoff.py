5d6850f09ab2a5ae841fde12f48304940737dc43
norihisayamada <sus444norihisa@gmail.com>
2020-05-31 11:43:01 +0900

    third commit

--- takeoff.py
+++ takeoff.py
@@ -1,4 +1,17 @@
-from dronekit import connect
-
+from dronekit import connect, VehicleMode
+import time
 
 vehicle = connect('127.0.0.1:14550', wait_ready=True)
+
+print("connected")
+print(vehicle)
+
+vehicle.armed = True
+
+
+# while not vehicle.mode.name == 'GUIDE' and not vehicle.armed:
+#     time.sleep(1)
+
+# vehicle.simple_takeoff
+# vehicle.groundspeed
+# vehicle.gimbal
