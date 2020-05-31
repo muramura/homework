70f908b2a0eb6e33be24129a0caeeb967228508c
norihisayamada <sus444norihisa@gmail.com>
2020-05-31 09:35:10 +0900

    second xommit

--- 0530.py
+++ 0530.py
@@ -9,6 +9,7 @@ from dronekit import connect, VehicleMode
 # Connect to the Vehicle.
 print("Connecting to vehicle on: %s" % (connection_string,))
 vehicle = connect(connection_string, wait_ready=True)
+vehicle.gimbal.rotate(-90,0,0)
 
 # Get some vehicle attributes (state)
 print "Get some vehicle attribute values:"
---
+++ takeoff.py
@@ -0,0 +1,4 @@
+from dronekit import connect
+
+
+vehicle = connect('127.0.0.1:14550', wait_ready=True)
